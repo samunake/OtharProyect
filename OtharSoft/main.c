ce8dc39bd866bcf768e3e2d79cb421638f68e828
Tomas Ariza <ldatac@hotmail.com>
2015-06-28 20:22:14 +0200

    Main limpio con comentarios

--- OtharSoft/main.c
+++ OtharSoft/main.c
@@ -8,46 +8,52 @@
 
 int main(void)
 {
+	// System Configuration Init
 	SystemInit();
-	Init_SysTick();  // Inicia Delay
+
+	// Init Systick for Delays
+	Init_SysTick();
+
+	// Rest LCD PC6 = 0
 	LCD_Reset();
+
+	// Load FSMC Config, GPIO Config, PLLs, LCD init values
 	LCD_Initialization();
 	Delay_ms(500);
+
+	// Init PWM on Timer 4 Channel 1 (PD12)
 	InitializePWM_TIM4();
+
 	Display_OFF();
 	Delay_ms(10);
 	Display_ON();
-	LCD_Clear(Red);
+
+
 	/*Memory_Clear();*/
-	Delay_ms(2500);
-	/*Text_color(Green); // Set the color
-	Geometric_Coordinate(100,200,200,400);
-	LCD_WriteReg(0x90,0xb0);*/
+	//Delay_ms(2500);
 
 
 
-
-
-	Active_Window(100,200,200,300);
-	//Graphic_Mode();
-	XY_Coordinate(100,200);
-	LCD_WriteCmd(0x02);//MRWC
-	uint32_t i;
-	for(i=0;i<10201;i++)
-	{
-		LCD_WriteData(Blue);
-		Chk_Busy();
-	}
-	Delay_ms(2000);
-	//ActiveWindow_Clear();
+			/*Active_Window(100,200,200,300);
+			//Graphic_Mode();
+			XY_Coordinate(100,200);
+			LCD_WriteCmd(0x02);//MRWC
+			uint32_t i;
+			for(i=0;i<10201;i++)
+			{
+				LCD_WriteData(Blue);
+				Chk_Busy();
+			}
+			Delay_ms(2000);
+			//ActiveWindow_Clear();*/
 
     while(1)
 
 
     {
-    	//temp = LCD_StatusRead();
 
-    	//Test();
+
+    	Test();
 
 
     }
